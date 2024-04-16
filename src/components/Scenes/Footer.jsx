b0acaf9cb35b2c0a78eaba82a98d14e8f63c94f4
JohnKamaujk <johnjkkamau@gmail.com>
2024-04-16 13:11:03 +0300

    :sparkles:feat:complete footer section

--- src/components/Scenes/Footer.jsx
+++ src/components/Scenes/Footer.jsx
@@ -12,11 +12,12 @@ const Footer = () => {
       variants={{
         hidden: { opacity: 0, y: 20 },
         visible: { opacity: 1, y: 0 },
-      }}>
+      }}
+    >
       <hr className="flex-grow border-t border-nd-white-1/10 w-[60%] sm:w-[50%] md:w-[40%] mb-3 sm:mb-4 md:mb-6" />
       <p className="text-sm md:text-base text-nd-third text-center max-w-[350px] sm:max-w-none">
-        Crafted by me, inspired by the exceptional web portfolio design of{" "}
-        <a href="https://brittanychiang.com/">Brittany Chiang.</a>
+        Built with a Dash of Wit and a Pinch of Panache: Because Who Says
+        Portfolios Can't Be Fun?🎨
       </p>
     </motion.footer>
   );
